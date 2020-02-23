8a02072c2c1823c7764a4c28a34c99acfcff85ec
Combatwombat22 <34546829+Combatwombat22@users.noreply.github.com>
2020-02-22 20:55:05 -0800

    Add files via upload

---
+++ Hello/src/module-info.java
@@ -0,0 +1,7 @@
+//class - Contains Everything - Has Members
+//methods - do something
+//properties - store something
+
+//access modifier (public) - Who can use?
+//static - No instance of class is needed
+//object - instance of a class
